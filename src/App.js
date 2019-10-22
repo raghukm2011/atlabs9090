6bf6b59819822ec7a7b462656c2fa539ffd50957
haampus <kannanramaswami@gmail.com>
2019-10-22 11:07:01 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Welcome from './components/welcome';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Welcome></Welcome>
+      </header>
+    </div>
+  );
+}
+
+export default App;
