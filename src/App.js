1d4c95cbcc2dda38a11e68f0c66e7693865bd6a6
haampus <kannanramaswami@gmail.com>
2019-10-22 11:14:01 +0530

    commit1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import './App.css';
-import Welcome from './components/welcome';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Welcome></Welcome>
+        hello world
       </header>
     </div>
   );
